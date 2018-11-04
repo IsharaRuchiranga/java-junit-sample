31403abdd2d52453b1becfced30043149ccbee17
IsharaRuchiranga <ishara.ira@gmail.com>
2018-11-04 22:09:46 +0530

    test 2

--- src/main/java/com/codecentric/sample/SampleApplication.java
+++ src/main/java/com/codecentric/sample/SampleApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("de.codecentric.sample")
 public class SampleApplication {
     public static void main(String[] args) {
-        //1
+        //2
             SpringApplication.run(SampleApplication.class, args);
         }
 }
